ba1be47d4cd60ed70db6de93b752878d9b6de7d9
lingha-anbu <lingha@vetstoria.com>
2018-12-27 12:03:49 +0530

    # Please include a useful commit message Lingha!

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
-if __name__ == "__main__":
-    print_hello_world()
-
-def print_hello_world():
+def hello_world():
     print('Hello World')
+
+def say_motto():
+    print('Victory, always and forever')
+
+if __name__ == "__main__":
+    hello_world()
+
+say_motto()
